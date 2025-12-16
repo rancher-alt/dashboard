383d949cd393e47134b654df8324c9459bf34f4c
Nancy Butler <42977925+mantis-toboggan-md@users.noreply.github.com>
2025-12-16 06:40:43 -0800

    vnode wip

--- pkg/imported/components/CruImported.vue
+++ pkg/imported/components/CruImported.vue
@@ -428,6 +428,7 @@ export default defineComponent({
     :done-route="doneRoute"
     :errors="errors"
     :validation-passed="fvFormIsValid"
+    :show-toc="true"
     @error="e=>errors=e"
     @finish="save"
   >
